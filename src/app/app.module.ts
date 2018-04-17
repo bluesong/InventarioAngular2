6efddd86e62969c9f5a93fd192d4ce3a68bf697a
Bluesong <julian-lagos@hotmail.com>
2018-04-17 09:30:04 -0500

    ajustes iniciales para parametrizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { VentasComponent } from './components/ventas/ventas.component';
 //sevicios
 import {AuthService} from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ParametrosComponent } from './components/parametros/parametros.component';
 
 
 
@@ -25,7 +26,8 @@ import { HttpClientModule } from '@angular/common/http';
     PreciosComponent,
     OcultaComponent,
     ProductosComponent,
-    VentasComponent
+    VentasComponent,
+    ParametrosComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,16 @@ import {HomeComponent} from './components/home/home.component';
 import {OcultaComponent} from './components/oculta/oculta.component';
 import {PreciosComponent} from './components/precios/precios.component';
 import {ProductosComponent} from './components/productos/productos.component';
+import { ParametrosComponent } from './components/parametros/parametros.component';
 import {VentasComponent} from './components/ventas/ventas.component';
 
 const app_routes: Routes = [
-  { path: 'home', component:HomeComponent},
+  { path: 'home', component: HomeComponent},
   { path: 'oculta', component: OcultaComponent},
   { path: 'precios', component: PreciosComponent},
   { path: 'productos', component: ProductosComponent},
-  { path: 'ventas',component:VentasComponent},
+  { path: 'ventas', component: VentasComponent},
+  { path: 'parametros', component: ParametrosComponent},
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
